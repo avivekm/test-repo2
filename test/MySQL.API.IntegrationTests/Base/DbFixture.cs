8f9b97905ccf3e7cfcc2978f20dc5a14524d84b5
avivekm <vivek.maurya@neosoftmail.com>
2022-01-12 15:43:18 +0530

    ports changes

--- test/MySQL.API.IntegrationTests/Base/DbFixture.cs
+++ test/MySQL.API.IntegrationTests/Base/DbFixture.cs
@@ -24,9 +24,9 @@ namespace MySQL.API.IntegrationTests.Base
         {
             var applicationBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             var identityBuilder = new DbContextOptionsBuilder<IdentityDbContext>();
-            ApplicationConnString = $"Server=localhost;Port=3306;Database={ApplicationDbName};Userid=root;Password=userpass;";
-                    IdentityConnString = $"Server=localhost;Port=3306;Database={IdentityDbName};Userid=root;Password=userpass;";
-                    HealthCheckConnString = $"Server=localhost=3306;Database={HealthCheckDbName};Userid=root;Password=userpass;";
+            ApplicationConnString = $"Server=localhost;Port=3307;Database={ApplicationDbName};Userid=root;Password=root;";
+                    IdentityConnString = $"Server=localhost;Port=3307;Database={IdentityDbName};Userid=root;Password=root;";
+                    HealthCheckConnString = $"Server=localhost=3307;Database={HealthCheckDbName};Userid=root;Password=root;";
                     applicationBuilder.UseMySql(ApplicationConnString, new MySqlServerVersion(new Version(8, 0, 11)),options=>options.EnableRetryOnFailure());
                     identityBuilder.UseMySql(IdentityConnString, new MySqlServerVersion(new Version(8, 0, 11)), options => options.EnableRetryOnFailure());
             _applicationDbContext = new ApplicationDbContext(applicationBuilder.Options);
